8f03c89f2eff920d670899957e82c9baef249c31
南 圭介 <bb10minami@gmail.com>
2024-12-23 10:00:22 +0900

    リファクタリング

--- Display/TransportInfo.xaml.cs
+++ Display/TransportInfo.xaml.cs
@@ -3,7 +3,6 @@ using ClassLibrary;
 using MaterialDesignThemes.Wpf;
 using Microsoft.Xaml.Behaviors.Core;
 using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Controls;
